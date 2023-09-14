c034c9501fe007359dfe0448b75d7ee019f9f6ba
chetansmali <cmali4596@gmail.com>
2023-09-14 22:06:39 +0530

    //A Sub-array with given sum code in Java is a program that  to a contiguous subsequence of elements within an array that has a sum equal to a specific target value. In other words, it is a consecutive sequence of elements in an array whose sum is equal to a given sum value. The goal is to find such a sub-array within the given array.

---
+++ ArrSumEqualEle.dart
@@ -0,0 +1,16 @@
+void main(){
+  var arr=[1, 2 ,3,2 ,10, 5];
+  int sum =7;
+
+  
+  for(int i=0;i<arr.length;i++){
+   int csum=0;
+      for(int j=i;j<arr.length;j++){
+        csum = csum +arr[j];
+        if(csum ==sum){
+          print("theg index $i and $j");
+        }
+      }
+  }
+  
+}
